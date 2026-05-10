f37eaad892fa24aa617e5a884f905e8a131f5082
ZahradPaw <zaharpaw005@gmail.com>
2026-05-10 16:21:35 +0500

    Add gunocorn.py to compose and add new favicon for site

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Авторизация по сессиям
-    path('api/v1/auth/', include('rest_framework.urls')),
+    #path('api/v1/auth/', include('rest_framework.urls')),
+    
     # Авторизация через JWT токены
     path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
