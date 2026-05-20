d50f99c6e9f64af40834ad663110e55a33e26298
ZahradPaw <zaharpaw005@gmail.com>
2026-05-20 23:55:21 +0500

    Fixed settings databases

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,18 +83,18 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': "django.db.backends.postgresql",
-    #    'NAME': os.environ.get('DATABASE_NAME'),
-    #    'USER': os.environ.get('DATABASE_USERNAME'),
-    #    'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-    #    'HOST': os.environ.get('DATABASE_HOST'),
-    #    'PORT': os.environ.get('DATABASE_PORT'),
-    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  
-        'NAME': BASE_DIR / 'db.sqlite3',  
+        'ENGINE': "django.db.backends.postgresql",
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USERNAME'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',  
+    #    'NAME': BASE_DIR / 'db.sqlite3',  
+    #}
 }
 
 
